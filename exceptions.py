df0e44fa71cea9b18974c5da7996e96367c59c41
Grant Ward <walter.grant.ward@gmail.com>
2017-02-19 21:47:23 -0500

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+"""
+Package: useless-exceptions
+Filename: exceptions.py
+Author(s): Grant W
+
+This module contains useless exceptions for "use" in your useless python app
+"""
+
+class ZeroSubtractionError(Exception):
+    """Make sure you don't subtract zero from zero ever again
+    """
+    pass
